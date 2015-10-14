d6187995e524a74ea5d0b8bcf017763aa9124f59
Chris <faschr08@evergreen.edu>
2015-10-14 10:59:21 -0700

    os-lab2

---
+++ os161/kern/startup/main.c
@@ -0,0 +1,73 @@
+Script started on Wed 14 Oct 2015 10:43:55 AM PDT
+Welcome to the Holodeck.
+
+	"I think they're going to take all this money that we spend now on war
+and death --"
+	"And make them spend it on life."
+		-- Edith Keeler and Kirk, "The City on the Edge of Forever",
+		   stardate unknown.
+
+[01;36mfaschr08[0;37m@[01;36mholodeck2[00m:[01;35mstartup[00m$ script
+Script started, file is typescript
+Welcome to the Holodeck.
+
+I thought my people would grow tired of killing.  But you were right,
+they see it is easier than trading.  And it has its pleasures.  I feel
+it myself.  Like the hunt, but with richer rewards.
+		-- Apella, "A Private Little War", stardate 4211.8
+
+[01;36mfaschr08[0;37m@[01;36mholodeck2[00m:[01;35mstartup[00m$ script
+Script started, file is typescript
+Welcome to the Holodeck.
+
+You!  What PLANET is this!
+		-- McCoy, "The City on the Edge of Forever", stardate 3134.0
+
+[01;36mfaschr08[0;37m@[01;36mholodeck2[00m:[01;35mstartup[00m$ help
+GNU bash, version 4.2.45(1)-release (x86_64-pc-linux-gnu)
+These shell commands are defined internally.  Type `help' to see this list.
+Type `help name' to find out more about the function `name'.
+Use `info bash' to find out more about the shell in general.
+Use `man -k' or `info' to find out more about commands not in this list.
+
+A star (*) next to a name means that the command is disabled.
+
+ job_spec [&]                            history [-c] [-d offset] [n] or hist>
+ (( expression ))                        if COMMANDS; then COMMANDS; [ elif C>
+ . filename [arguments]                  jobs [-lnprs] [jobspec ...] or jobs >
+ :                                       kill [-s sigspec | -n signum | -sigs>
+ [ arg... ]                              let arg [arg ...]
+ [[ expression ]]                        local [option] name[=value] ...
+ alias [-p] [name[=value] ... ]          logout [n]
+ bg [job_spec ...]                       mapfile [-n count] [-O origin] [-s c>
+ bind [-lpvsPVS] [-m keymap] [-f filen>  popd [-n] [+N | -N]
+ break [n]                               printf [-v var] format [arguments]
+ builtin [shell-builtin [arg ...]]       pushd [-n] [+N | -N | dir]
+ caller [expr]                           pwd [-LP]
+ case WORD in [PATTERN [| PATTERN]...)>  read [-ers] [-a array] [-d delim] [->
+ cd [-L|[-P [-e]]] [dir]                 readarray [-n count] [-O origin] [-s>
+ command [-pVv] command [arg ...]        readonly [-aAf] [name[=value] ...] o>
+ compgen [-abcdefgjksuv] [-o option]  >  return [n]
+ complete [-abcdefgjksuv] [-pr] [-DE] >  select NAME [in WORDS ... ;] do COMM>
+ compopt [-o|+o option] [-DE] [name ..>  set [-abefhkmnptuvxBCHP] [-o option->
+ continue [n]                            shift [n]
+ coproc [NAME] command [redirections]    shopt [-pqsu] [-o] [optname ...]
+ declare [-aAfFgilrtux] [-p] [name[=va>  source filename [arguments]
+ dirs [-clpv] [+N] [-N]                  suspend [-f]
+ disown [-h] [-ar] [jobspec ...]         test [expr]
+ echo [-neE] [arg ...]                   time [-p] pipeline
+ enable [-a] [-dnps] [-f filename] [na>  times
+ eval [arg ...]                          trap [-lp] [[arg] signal_spec ...]
+ exec [-cl] [-a name] [command [argume>  true
+ exit [n]                                type [-afptP] name [name ...]
+ export [-fn] [name[=value] ...] or ex>  typeset [-aAfFgilrtux] [-p] name[=va>
+ false                                   ulimit [-SHacdefilmnpqrstuvx] [limit>
+ fc [-e ename] [-lnr] [first] [last] o>  umask [-p] [-S] [mode]
+ fg [job_spec]                           unalias [-a] name [name ...]
+ for NAME [in WORDS ... ] ; do COMMAND>  unset [-f] [-v] [name ...]
+ for (( exp1; exp2; exp3 )); do COMMAN>  until COMMANDS; do COMMANDS; done
+ function name { COMMANDS ; } or name >  variables - Names and meanings of so>
+ getopts optstring name [arg]            wait [id]
+ hash [-lr] [-p pathname] [-dt] [name >  while COMMANDS; do COMMANDS; done
+ help [-dms] [pattern ...]               { COMMANDS ; }
+[01;36mfaschr08[0;37m@[01;36mholodeck2[00m:[01;35mstartup[00m$ help[Khel
